7b35ad9b9f3a6982fcf619c276247dfc77d3d6ea
Karni Shekhawat <Karnisinghshekhawat@protonmail.com>
2021-08-26 16:59:11 +0200

    Some error settings

---
+++ src/WebPortal_PDFs/Controllers/AutenticationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPortal_PDFs.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    public class AutenticationController : ControllerBase
+    {
+        public AutenticationController()
+        {
+
+        }
+
+           
+    }
+}
